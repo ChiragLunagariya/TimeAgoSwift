0ea0f1b1e935fc67eaaf9726bb455072fbc079c0
SOTSYS220 <sotsys220@sotsys220s-mac-mini.local>
2017-05-05 18:26:22 +0530

    - Swift 3.0

--- TimeAgo/TimeAgo.swift
+++ TimeAgo/TimeAgo.swift
@@ -14,37 +14,36 @@ public class TimeAgo: NSObject {
         print("TimeAgo Framework")
     }
 
-    public func timeAgoStringFromDate(date: NSDate) -> NSString? {
-        let formatter = NSDateComponentsFormatter()
-        formatter.unitsStyle = .Full
+    public func timeAgoStringFromDate(date: Date) -> String? {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
         
-        let now = NSDate()
+        let now = Date()
         
-        let calendar = NSCalendar.currentCalendar()
-        let components = calendar.components([NSCalendarUnit.Year, .Month, .WeekOfMonth, .Day, .Hour, .Minute, .Second],
-            fromDate: date,
-            toDate: now,
-            options:NSCalendarOptions(rawValue: 0))
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .weekOfMonth, .day, .hour, .minute, .second], from: date, to: now)
         
-        if components.year > 0 {
-            formatter.allowedUnits = .Year
-        } else if components.month > 0 {
-            formatter.allowedUnits = .Month
-        } else if components.weekOfMonth > 0 {
-            formatter.allowedUnits = .WeekOfMonth
-        } else if components.day > 0 {
-            formatter.allowedUnits = .Day
-        } else if components.hour > 0 {
-            formatter.allowedUnits = .Hour
-        } else if components.minute > 0 {
-            formatter.allowedUnits = .Minute
+       
+        
+        if components.year! > 0 {
+            formatter.allowedUnits = .year
+        } else if components.month! > 0 {
+            formatter.allowedUnits = .month
+        } else if components.weekOfMonth! > 0 {
+            formatter.allowedUnits = .weekOfMonth
+        } else if components.day! > 0 {
+            formatter.allowedUnits = .day
+        } else if components.hour! > 0 {
+            formatter.allowedUnits = .hour
+        } else if components.minute! > 0 {
+            formatter.allowedUnits = .minute
         } else {
-            formatter.allowedUnits = .Second
+            formatter.allowedUnits = .second
         }
         
         let formatString = NSLocalizedString("%@ ago", comment: "Used to say how much time has passed. e.g. '2 hours ago'")
         
-        guard let timeString = formatter.stringFromDateComponents(components) else {
+        guard let timeString = formatter.string(from: components) else {
             return nil
         }
         return String(format: formatString, timeString)
